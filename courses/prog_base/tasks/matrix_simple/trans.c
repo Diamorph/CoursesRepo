74f1ebfbc3938c4c0eafe0c46c71ba636a43acbb
Diamorph <diamorph1309@gmail.com>
2015-10-23 18:16:21 +0300

    1

--- courses/prog_base/tasks/matrix_simple/trans.c
+++ courses/prog_base/tasks/matrix_simple/trans.c
@@ -1,28 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
-#include <math.h>
 
-/*void fillRand();
-void rotateCW180();
-void flipV();
-void transposSide();
+
+/*void fillRand(int mat[4][4]);
+void rotateCW180(int mat[4][4]);
+void flipV(int mat[4][4]);
+void transposSide(int mat[4][4]);
+void printMatrix(int mat[4][4]);
 int main()
-{
-    fillRand();
-    rotateCW180();
-    flipV();
-    transposSide();
+{ int mat[4][4];
+    fillRand(mat);
+    printMatrix(mat);
+
+    rotateCW180(mat);
+    printMatrix(mat);
+
+    flipV(mat);
+    printMatrix(mat);
+
+    transposSide(mat);
+    printMatrix(mat);
+
     system("pause");
     return 0;
-}*/
+}
 
-
-
+void printMatrix(int mat[4][4]) {
+   int i, j;
+    int n = 4;
+    printf("{ ");
+    for (i = 0; i < n; i++) {
+        printf("{");
+        for (j = 0; j < n; j++) {
+            printf("%5i, ", mat[i][j]);
+        }
+        printf("}");
+        if ( i < n - 1)
+            printf("\n  ");
+    }
+    printf(" };\n");
+}
+*/
 void fillRand(int mat[4][4])
 {
-int i, j, p, pp;
-srand(time(NULL));
+int i, j;;
     for( i = 0; i < 4; i++)
     {
         for( j = 0; j < 4; j++)
@@ -30,79 +50,70 @@ srand(time(NULL));
             mat[i][j] = rand() % 1999 - 999; // -999 to 999
         }
     }
-    }
+}
 
 
 
     void rotateCW180(int mat[4][4])
 {
-int i,j,work,p,pp;
-for ( i = 0; i < 4; i++)
-    {
-work = mat[i][0];
-mat[i][0] = mat [i][3];
-mat[i][3] = work;
-}
-for ( i = 0; i < 4; i++)
-    {
-work = mat[i][1];
-mat[i][1] = mat[i][2];
-mat[i][2] = work;
-}
-for ( j = 0; j < 4; j++)
-    {
-work = mat[0][j];
-mat[0][j] = mat[3][j];
-mat[3][j] = work;
-}
-for ( j = 0; j < 4; j++)
-    {
-work = mat[1][j];
-mat[1][j] = mat[2][j];
-mat[2][j] = work;
-}
+int i,j,p,pp;
+int mat1[4][4];
+for (i = 0; i < 4; i++)
+{
+    for (j = 0; j < 4; j++){
+        mat1[i][j] = mat[i][j];
+    }
 }
 
+for (i = 0; i < 4; i++){
+    for (j = 0; j < 4; j++){
+        p = 3 - i;
+        pp = 3 - j;
+        mat[i][j] = mat1[p][pp];
+                }
+            }
+}
 
 
 void flipV(int mat[4][4])
 {
-    int j,work,p,pp;
-for ( j = 0; j < 4; j++)
-    {
-work = mat[0][j];
-mat[0][j] = mat [3][j];
-mat[3][j] = work;
+    int i,j,c;
+    int mat1[4][4];
+for (i = 0; i < 4; i++)
+{
+    for (j = 0; j < 4; j++){
+        mat1[i][j] = mat[i][j];
+    }
 }
-for ( j = 0; j < 4; j++)
-    {
-work = mat[1][j];
-mat[1][j] = mat [2][j];
-mat[2][j] = work;
+for (i = 0; i < 4; i++)
+{
+    for (j = 0; j < 4; j++){
+        c = 3 - i;
+        mat[i][j] = mat1[c][j];
+    }
 }
 }
-
 
 
 void transposSide(int mat[4][4])
 {
-int i,j,t,t1,t2,t3,t4,t5,t6,p,pp;
-    t = mat[0][0];
-    mat[0][0] = mat[3][3];
-    mat[3][3] = t;
-    t1 = mat[1][0];
-    mat[1][0] = mat[3][2];
-    mat[3][2] = t1;
-    t2 = mat[2][0];
-    mat[2][0] = mat[3][1];
-    mat[3][1] = t2;
-    t3 = mat[0][1];
-    mat[0][1] = mat[2][3];
-    mat[2][3] = t3;
-    t4 = mat[1][1];
-    mat[1][1] = mat[2][2];
-    mat[2][2] = t4;
-    t5 = mat[0][2];
-    mat[0][2] = mat[1][3];
-    mat[1][3] = t5;
+int i, j, c, d;
+int mat1[4][4];
+    for (i = 0; i < 4; i++)
+{
+    for (j = 0; j < 4; j++){
+        mat1[i][j] = mat[i][j];
+    }
 }
+    for (i = 0; i < 4; i++)
+        {
+        for (j = 0; j < 4; j++){
+            c = 3 - j;
+            d = 3 - i;
+            mat[i][j] = mat1[c][d];
+        }
+    }
+}
+
+
+
