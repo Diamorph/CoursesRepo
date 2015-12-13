7e6d471bfa5378e9f68f6b2177735f023fefeb4a
Diamorph <diamorph1309@gmail.com>
2015-12-13 19:53:26 +0200

    recursion

---
+++ courses/prog_base/tasks/recursion/main.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int lastDigitCode(const char * pStart, const char * pChar) {
+    if ((pStart > pChar) &&(!isxdigit(*pChar)))        return '0';// випадок 1, пуста строка або в строкі немає шістнадцяткової цифри
+    else if (isxdigit(*pChar))   return (int)*pChar;  // випадок 2,  знайшли цифру
+    else                        return lastDigitCode(pStart, pChar - 1); // правило
+}
+
+int main(){
+	char str[100];
+	gets(str);
+	printf("result is  %c\n ", lastDigitCode(str, str + strlen(str) - 1));
+	getchar();
+	return EXIT_SUCCESS;
+}
