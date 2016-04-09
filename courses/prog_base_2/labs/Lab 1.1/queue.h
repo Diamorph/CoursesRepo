763b036ab5285ae725e8e3462d8680f8c2ba386e
Diamorph <diamorph1309@gmail.com>
2016-04-09 13:18:44 +0300

    Lab 1

---
+++ courses/prog_base_2/labs/Lab 1.1/queue.h
@@ -0,0 +1,30 @@
+#ifndef LIST_H_INCLUDED1
+#define LIST_H_INCLUDED1
+
+
+#include "queue.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct queue_s queue_t;
+
+struct command {
+    player_cmd command;
+    player_t * players;
+};
+
+typedef enum {
+    KK = 0,
+    ERROR
+} queue_status;
+
+
+queue_t * queue_new();
+queue_status queue_add(queue_t * self, struct command com);
+struct command queue_del(queue_t * self);
+void queue_remove (queue_t ** self);
+
+
+#endif
