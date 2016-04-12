f2e7975ab482b147f71c83b1ffe75351d3ca8182
Diamorph <diamorph1309@gmail.com>
2016-04-12 11:41:15 +0300

    map
    + map

--- courses/prog_base_3/project/main.cpp
+++ courses/prog_base_3/project/main.cpp
@@ -1,20 +1,19 @@
-#include <iostream> 
 #include <SFML/Graphics.hpp>
-
+#include <iostream> 
+#include "map.h" 
 
 using namespace sf;
 
-
-class Player { 
+class Player {
 public:
-	float x, y, w, h, dx, dy, speed = 0; 
-	int dir = 0; 
-	String File; 
+	float x, y, w, h, dx, dy, speed;
+	int dir;
+	String File;
 	Image image;
 	Texture texture;
 	Sprite sprite;
-
-	Player(String F, float X, float Y, float W, float H){  
+	Player(String F, float X, float Y, float W, float H){
+		dx = 0; dy = 0; speed = 0; dir = 0;
 		File = F;
 		w = W; h = H;
 		image.loadFromFile("images/" + File);
@@ -22,26 +21,23 @@ public:
 		texture.loadFromImage(image);
 		sprite.setTexture(texture);
 		x = X; y = Y;
-		sprite.setTextureRect(IntRect(0, 0, w, h));  
+		sprite.setTextureRect(IntRect(0, 0, w, h));
 	}
-
-
-
 	void update(float time)
 	{
 		switch (dir)
 		{
-		case 0: dx = speed; dy = 0;   break;
-		case 1: dx = -speed; dy = 0;   break;
-		case 2: dx = 0; dy = speed;   break;
-		case 3: dx = 0; dy = -speed;   break;
+		case 0: dx = speed; dy = 0; break;
+		case 1: dx = -speed; dy = 0; break;
+		case 2: dx = 0; dy = speed; break;
+		case 3: dx = 0; dy = -speed; break;
 		}
 
 		x += dx*time;
 		y += dy*time;
 
 		speed = 0;
-		sprite.setPosition(x, y); 
+		sprite.setPosition(x, y);
 	}
 };
 
@@ -50,10 +46,18 @@ int main()
 {
 	RenderWindow window(sf::VideoMode(640, 480), "Chris Jumper");
 
-	float CurrentFrame = 0;
-	Clock clock;
+	Image map_image;
+	map_image.loadFromFile("images/map.png");
+	Texture map;
+	map.loadFromImage(map_image);
+	Sprite s_map;
+	s_map.setTexture(map);
+
 
 	Player p("hero.png", 250, 250, 96.0, 96.0);
+
+	float CurrentFrame = 0;
+	Clock clock;
 
 	while (window.isOpen())
 	{
@@ -70,42 +74,54 @@ int main()
 				window.close();
 		}
 
-
-		
 		if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))) {
 			p.dir = 1; p.speed = 0.1;
 			CurrentFrame += 0.005*time;
 			if (CurrentFrame > 3) CurrentFrame -= 3;
-			p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 96, 96, 96)); 
+			p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 96, 96, 96));
 		}
 
 		if ((Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)))) {
 			p.dir = 0; p.speed = 0.1;
 			CurrentFrame += 0.005*time;
 			if (CurrentFrame > 3) CurrentFrame -= 3;
-			p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 192, 96, 96));  
+			p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 192, 96, 96));
 		}
 
 		if ((Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W)))) {
 			p.dir = 3; p.speed = 0.1;
 			CurrentFrame += 0.005*time;
 			if (CurrentFrame > 3) CurrentFrame -= 3;
-			p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 288, 96, 96));  
+			p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 288, 96, 96));
 
 		}
 
-		if ((Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)))) { 
+		if ((Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)))) {
 			p.dir = 2; p.speed = 0.1;
-			CurrentFrame += 0.005*time; 
-			if (CurrentFrame > 3) CurrentFrame -= 3; 
-			p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96)); 
+			CurrentFrame += 0.005*time;
+			if (CurrentFrame > 3) CurrentFrame -= 3;
+			p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));
 
 		}
-
 		p.update(time);
 
 
 		window.clear();
+
+		for (int i = 0; i < HEIGHT_MAP; i++)
+		for (int j = 0; j < WIDTH_MAP; j++)
+		{
+			if (TileMap[i][j] == ' ')  s_map.setTextureRect(IntRect(0, 0, 32, 32)); 
+			if (TileMap[i][j] == 's')  s_map.setTextureRect(IntRect(32, 0, 32, 32));
+			if ((TileMap[i][j] == '0')) s_map.setTextureRect(IntRect(64, 0, 32, 32));
+
+
+			s_map.setPosition(j * 32, i * 32);
+
+			window.draw(s_map);
+		}
+
+
 		window.draw(p.sprite);
 		window.display();
 	}
