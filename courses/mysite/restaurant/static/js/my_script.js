96c9fb3e2823638fd895718119a1c14a114efd7f
Diamorph <diamorph1309@gmail.com>
2016-12-16 01:02:44 +0200

    ajax
    full ajax to search

--- courses/mysite/restaurant/static/js/my_script.js
+++ courses/mysite/restaurant/static/js/my_script.js
@@ -9,11 +9,10 @@ $(document).ready(function(){
             success: function(result){
 
                result = jQuery.parseJSON(result);
-             alert(result);
+             //alert(result);
             $(".main_content").remove();
-            for(i in result){
-                $("#new_content").append
-                ('<p><img src = "'+result[i].url+''+result[i].image.url+'"></p><a href = "/restaurant/cold_dish/'+ result[i].id + '" ><p>Назва'+result[i].name+'</p></a><p>Склад :"'+result[i].consist+'"</p><p>Вага:'+result[i].weight+'</p><p>Ціна :'+result[i].price+'</p><form action = "/restaurant/cold_dish/del/'+ result[i].id +'/" method = "POST"><input class  ="button" type = "submit"  value = "Видалити"></form>');
+            for(i in result)
+            { $("#new_content").append(' <p><img src = "'+result[i].image+'"></p><a href = "/restaurant/cold_dish/' +result[i].id +' " ><p>Назва :'+result[i].name+'</p></a><p>Склад :'+result[i].consist+'</p><p>Вага: '+result[i].weight +'</p><p>Ціна :'+result[i].price+'</p><form action = "/restaurant/cold_dish/del/'+result[i].id+'/" method = "POST"><input class  ="button" type = "submit"  value = "Видалити"></form>');
             };
            
         }});
